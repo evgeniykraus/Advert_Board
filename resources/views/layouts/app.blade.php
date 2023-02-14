e4749a73ef971b925eb0d0832b23dc0d7d5f05c7
Краус Евгений Владимирович <evgeniykraus@gmail.com>
2023-02-14 19:32:27 +0700

    Добавлены шаблоны

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,41 +12,8 @@
 <header>
     <!-- header content here -->
 </header>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03"
-            aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li class="nav-item">
-                <a class="nav-link" href="{{route('home')}}">Главная <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{route('categories')}}">Категории <span class="sr-only">(current)</span></a>
-            </li>
-        </ul>
-        <ul class="navbar-nav ml-auto">
-            @auth
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('profile')}}">{{ Auth::user()->name }}</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('logout')}}">Выход</a>
-                </li>
-            @endauth
-            @guest
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('login')}}">Вход</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('register')}}">Регистрация</a>
-                </li>
-            @endguest
-        </ul>
-    </div>
-</nav>
-<main class="container my-5">
+@include('layouts.nav-bar')
+<main class="container my-5 col-12 col-md-12">
     @yield('content')
 </main>
 <footer>
--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -1,21 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-
     <div class="container mt-5">
         @if($message = session()->get('message', null))
             @include('layouts.alert')
         @endif
 
-        <div class="row">
+        <div class="row" style="margin-bottom: 30px">
             <div class="col-md-3">
-                <img src="" class="img-fluid rounded-circle" alt="User Photo">
+                <img src="/img/avatar.png" class="img-fluid rounded-circle" alt="User Photo">
             </div>
-            <div class="col-md-9">
-                <h3>Имя пользователя</h3>
-                <p>Email пользователя</p>
+            <div class="col-md-4">
+                <h3>{{$user['name'] .' '. $user['surname']}}</h3>
+                <ul class="list-group">
+                    <li class="list-group-item"><b>Email:</b> {{$user['email']}}</li>
+                    <li class="list-group-item"><b>Телефон:</b> {{$user['phone']}}</li>
+                    <li class="list-group-item"><b>Дата регистрации:</b> {{$user['registerDate']}}</li>
+                    <a href="{{route('admin_panel')}}" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Админка</a>
+                </ul>
             </div>
         </div>
+        @include('user.adverts')
     </div>
 
+    <script>
+        $('#myList a').on('click', function (e) {
+            e.preventDefault()
+            $(this).tab('show')
+        })
+    </script>
 @endsection
