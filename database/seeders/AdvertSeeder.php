53051c3c79cca057a8195c25c46cf96d7c49d4e8
evgeniykraus <evgeniykraus@gmail.com>
2023-02-14 23:13:43 +0700

    Добавил сидер для объявлений

---
+++ database/seeders/AdvertSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Advert;
+use Illuminate\Database\Seeder;
+
+class AdvertSeeder extends Seeder
+{
+    public function run()
+    {
+        for ($i = 1; $i < 21; $i++) {
+            Advert::factory()->create([
+                'title' => "Квартира_$i",
+                'description' => "Тестовое описание_$i",
+                'category_id' => 2,
+                'creator_id' => 1,
+                'price' => rand(10000, 2000000),
+            ]);
+            Advert::factory()->create([
+                'title' => "Машина_$i",
+                'description' => "Тестовое описание_$i",
+                'category_id' => 7,
+                'creator_id' => 1,
+                'price' => rand(100, 20000),
+            ]);
+        }
+    }
+}
