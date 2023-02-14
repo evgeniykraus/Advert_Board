6c0f99fcdd0beb8069bdb910bd46f1bc1e10c419
Краус Евгений Владимирович <evgeniykraus@gmail.com>
2023-02-14 19:38:06 +0700

    Добавлен посредник для вывода категорий товара в нав бар

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'categories' => \App\Http\Middleware\LoadCategoriesMiddleware::class,
     ];
 }
