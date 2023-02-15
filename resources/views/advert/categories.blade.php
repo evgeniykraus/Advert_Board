9c20873495e13a05d0f4d174bfead01542dede79
Краус Евгений Владимирович <evgeniykraus@gmail.com>
2023-02-15 16:46:23 +0700

    Теперь селектор не слетает при ошибке

--- resources/views/advert/categories.blade.php
+++ resources/views/advert/categories.blade.php
@@ -1,13 +1,12 @@
 @foreach ($categories as $category)
-
-    <option value="none" selected disabled hidden>Выберите категорию товара</option>
-
     @if ($category->children->isNotEmpty())
-        <option disabled >--{{$category->name}}--</option>
-        @include('advert.categories', ['categories' => $category->children])
+        <optgroup label="{{$category->name}}">
+            @include('advert.categories', ['categories' => $category->children])
+        </optgroup>
     @else
-        <option value="{{$category->id}}">{{$category->name}}</option>}}
+        <option value="{{$category->id}}" @if($category->id == old('category_id')) selected @endif>
+            {{$category->name}}
+        </option>
     @endif
-
 @endforeach
 
