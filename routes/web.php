01fd3d31a306ad8eb731580719758b5f94b0d2fd
Краус Евгений Владимирович <evgeniykraus@gmail.com>
2023-02-16 15:53:27 +0700

    К роутам добавил префиксы

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
-
 Route::middleware(['categories', 'user_on_black_list'])->group(function () {
 
 //Home_Page
@@ -15,18 +14,26 @@ Route::middleware(['categories', 'user_on_black_list'])->group(function () {
     Route::get('/search', [AdvertController::class, 'search'])->name('search');
 
 //Auth
-    Route::get('/login', [AuthController::class, 'showLoginForm'])->middleware('guest')->name('login');
-    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::prefix('/login')->group(function () {
+        Route::get('/', [AuthController::class, 'showLoginForm'])->middleware('guest')->name('login');
+        Route::post('/', [AuthController::class, 'login'])->name('login');
+    });
+
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
 //Register
-    Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
-    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::prefix('/register')->group(function () {
+        Route::get('/', [AuthController::class, 'showRegistrationForm'])->name('register');
+        Route::post('/', [AuthController::class, 'register'])->name('register');
+    });
 
 //Adverts
-    Route::get('/advert/{id}', [AdvertController::class, 'show'])->name('advert')->whereNumber('id');
-    Route::get('/advert/add', [AdvertController::class, 'create'])->middleware('auth')->name('add_advert');
-    Route::post('/advert/add', [AdvertController::class, 'store'])->middleware('auth')->name('add_advert');
+    Route::prefix('/advert')->group(function () {
+        Route::get('/{id}', [AdvertController::class, 'show'])->name('advert')->whereNumber('id');
+        Route::get('/add', [AdvertController::class, 'create'])->middleware('auth')->name('add_advert');
+        Route::post('/add', [AdvertController::class, 'store'])->middleware('auth')->name('add_advert');
+    });
+
 
 //Categories
     Route::get('/category/{id}', [CategoryController::class, 'show'])->name('category');
@@ -36,16 +43,16 @@ Route::middleware(['categories', 'user_on_black_list'])->group(function () {
     Route::get('/home', [UserController::class, 'show'])->name('profile')->middleware('auth');
 
     Route::middleware(['auth', 'admin'])->group(function () {
-        Route::get('/home/admin_panel', [UserController::class, 'adminPanel'])->name('admin_panel');
-        Route::get('/home/admin_panel/users', [UserController::class, 'usersList'])->name('users');
-        Route::post('/home/admin_panel/users', [UserController::class, 'blockUser'])->name('users');
-        Route::get('/home/admin_panel/user/edit', [UserController::class, 'edit'])->name('edit');
-        Route::post('/home/admin_panel/user/edit', [UserController::class, 'update'])->name('update');
-        Route::get('/home/admin_panel/adverts', [AdvertController::class, 'advertsToCheck'])->name('adverts_to_check');
-        Route::post('/home/admin_panel/adverts', [AdvertController::class, 'approve'])->name('advert_approve');
+        Route::prefix('/home/admin_panel')->group(function () {
+            Route::get('/home/admin_panel', [UserController::class, 'adminPanel'])->name('admin_panel');
+            Route::get('/users', [UserController::class, 'usersList'])->name('users');
+            Route::post('/users', [UserController::class, 'blockUser'])->name('users');
+            Route::get('//edit', [UserController::class, 'edit'])->name('edit');
+            Route::post('/edit', [UserController::class, 'update'])->name('update');
+            Route::get('/adverts', [AdvertController::class, 'advertsToCheck'])->name('adverts_to_check');
+            Route::post('/adverts', [AdvertController::class, 'approve'])->name('advert_approve');
+        });
     });
-
-
 });
 
 
